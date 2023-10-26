76f3a1ce8cee90f0fef895ced1b149406df79ca1
SheltaV <bluegiraffezaoz@gmail.com>
2023-10-26 18:00:51 +0300

    update-hw-ts-03

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,42 @@
+class Key {
+    private signature: number;
+    constructor() {
+        this.signature = Math.random()
+    }
+    getSignature(): number {
+        return this.signature
+    }
+}
+
+class Person {
+    constructor(private key: Key) { }
+    getKey(): Key {
+        return this.key
+    }
+}
+
+abstract class House {
+    protected door: boolean;
+    protected tenants: Person[] = [];
+    constructor(protected key: Key) {
+        this.door = false
+    }
+    comeIn(person: Person) {
+        if (this.door) {
+    this.tenants.push(person)
+        }
+    }
+    abstract openDoor(key: Key): void
+}
+
+class MyHouse extends House {
+openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+        this.door = true
+    }
+}
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
